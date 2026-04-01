426f0fd8d3b076c868b6ded86b1b69b131be8145
ispardha <chandelispardha@gmail.com>
2026-04-01 19:59:01 +0530

    images

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,8 +1,35 @@
-c.onst Navbar = () => {
+import Container from "../Container";
+import Logo from "./Logo";
+
+const Navbar = () => {
     return (
-        <div>
-            I am a Navbar
-        </div>
+        <div className="fixed w-full bg-white z-10 shadow-sm" >
+            <div 
+            className="
+            py-4
+            border-b-[1px]
+            ">
+                <Container>
+                    <div className="
+                    flex
+                    flex-row
+                    items-center
+                    justify-between
+                    gap-3
+                    md:gap-0
+                    "
+                    >
+                        <Logo />
+                        
+
+
+                    </div>
+
+                </Container >
+            </div>
+         </div>
     )
 
+
 }
+export default Navbar;
